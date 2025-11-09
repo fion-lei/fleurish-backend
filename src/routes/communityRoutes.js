644a853b9e8553f86750e6d99e803ad9b26290c0
fion-lei <137603599+fion-lei@users.noreply.github.com>
2025-11-09 03:44:13 -0700

    Revert "Add community leaderboard and ranking endpoints"
    This reverts commit 95010fc18633482425776184585e12ed3751c2d1.

--- src/routes/communityRoutes.js
+++ src/routes/communityRoutes.js
@@ -1,24 +1,9 @@
 // src/routes/communityRoutes.js
 const express = require('express');
 const router = express.Router();
-const {
-  getAllCommunities,
-  getCommunityById,
-  updateCommunityPoints,
-  getTopCommunities,
-  getUserCommunityRank,
-  getLeaderboardWithUserRank
-} = require('../controllers/communityController');
-const { protect } = require('../controllers/userController');
+const {getAllCommunities, getCommunityById, updateCommunityPoints} = require('../controllers/communityController');
 
-// Public routes
 router.get('/', getAllCommunities);
-router.get('/leaderboard/top', getTopCommunities);
-
-// Protected routes (require authentication)
-router.get('/leaderboard/me', protect, getUserCommunityRank);
-router.get('/leaderboard/combined', protect, getLeaderboardWithUserRank);
-
 router.get('/:id', getCommunityById);
 router.patch('/:id/points', updateCommunityPoints);
 
