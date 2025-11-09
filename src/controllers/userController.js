59742cfac38a6c05c201cb5fc9f58a6caaaa8116
aeMyst <panpetertran123@gmail.com>
2025-11-08 17:22:31 -0700

    test

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,5 @@
 const User = require('../models/User');
 
-// @desc    Generate a unique user ID
-// @access  Private
-const generateUniqueId = async () => {
-  const prefix = 'U'; // U for User
-  const timestamp = Date.now().toString(36); // Convert timestamp to base36
-  const randomStr = Math.random().toString(36).substr(2, 5); // 5 random chars
-  const proposedId = `${prefix}${timestamp}${randomStr}`.toUpperCase();
-  
-  // Check if ID already exists
-  const existingUser = await User.findOne({ userId: proposedId });
-  if (existingUser) {
-    // If ID exists, try again recursively
-    return generateUniqueId();
-  }
-  
-  return proposedId;
-};
-
 // @desc    Get user points (gems and coins)
 // @route   GET /api/users/:userId/points
 // @access  Public
@@ -53,7 +35,6 @@ exports.createUser = async (req, res) => {
   try {
     const {  email } = req.body;
     const newUser = new User({
-      userId: generateUniqueId(), // Assume a function to generate unique IDs
       email,
       gems: 0,
       coins: 0
